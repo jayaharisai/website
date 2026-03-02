df87d3fa7c6942b6f9fd5094b5d41a7145985744
Jayaharisai <jayaharisai1212@gmail.com>
2026-03-02 23:04:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/website" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
