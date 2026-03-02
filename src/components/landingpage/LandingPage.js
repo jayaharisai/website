9681a9b2e71955d32796fcf5bbbf1b14b606cb06
Jayaharisai <jayaharisai1212@gmail.com>
2026-03-02 22:58:34 +0530

    Initial set up done

---
+++ src/components/landingpage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./LandingPage.css"
+
+function LandingPage() {
+  return (
+    <div>
+        <h1>Welcome!</h1>
+        <p>This page is currently under construction.</p>
+        <p>Please check back soon.</p>
+    </div>
+  )
+}
+
+export default LandingPage
